620c01fc89098ad76a498888b449352b56c146b7
Jared Addison <77221122+jaredaddison@users.noreply.github.com>
2021-08-17 23:17:27 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './tailwind.css'
 import App from './App.vue'
 import { routes } from './routes.js'
 import { createRouter, createWebHistory } from 'vue-router'
+import { MotionPlugin } from '@vueuse/motion'
 
 const app = createApp(App)
 
@@ -12,4 +13,5 @@ const router = createRouter({
 })
 
 app.use(router)
+app.use(MotionPlugin)
 app.mount('#app')
