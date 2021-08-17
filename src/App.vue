f8bb694faf3238bc5ced175fcc63e1e5584b2814
Jared Addison <77221122+jaredaddison@users.noreply.github.com>
2021-08-17 23:14:39 +0800

    App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <header class="bg-white shadow" v-if="$route.meta.title">
+    <header
+class="bg-white text-gray-800 dark:bg-gray-900 dark:bg-gray-100 shadow"
+v-if="$route.meta.title">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold leading-tight text-gray-900">{{ $route.meta.title }}</h1>
       </div>
