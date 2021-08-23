c77697245f478cce343649be705c9a86feab2c5f
Jared Addison <drjaredaddison@gmail.com>
2021-08-23 05:26:02 +0000

    2

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,30 @@
 <template>
-<div v-if="MinimumDataBase">
-<MinimumDatabase />
+<div id="input">
+ <ul>
+  <li>
+<input v-model="dog.weight" type="number" placeholder="Weight" />kg
+  </li><li>
+<input v-model="dog.dehydration" type="number" placeholder="Dehydration"/>% bodyweight (5% Minor | 10% Moderate | 15% Severe)
+</li><li>
+<input v-model="dog.ongoingLoss" type="number" placeholder="Ongoin Loss"/>ml/hr of ongoing fluid loss
+</li><li>
+<input v-model="dog.bolusNumber" type="number" /> = Total Number of bolus' provided
+</li>
+</ul>
+</div>
+<div id="fluid">
+<p>
+ <ul>
+<li> Surgical fluid rate is = {{ dog.surgicalRate }}ml/hr </li>
+<li> A bolus for this patient is {{ dog.bolusVolume }}ml over 15minutes</li>
+<li> The maintenance fluid rate is {{ dog.maintenanceRate }}ml/hr</li>
+</ul>
+</p>
 </div>
 </template>
 
 <script setup>
-import MinimumDatabase from '@/components/MinimumDatabase.vue'
-const MinimumDataBase = true
+	import dog from '@/logic/Fluids'
 </script>
 
 <style scoped></style>
