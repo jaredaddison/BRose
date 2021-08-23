dc1bd0fa1480fe23fc7699c655c1d18dd441c440
Jared Addison <drjaredaddison@gmail.com>
2021-08-23 02:07:52 +0000

    Pricing

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
-  <div>
-    <header
-class="bg-white text-gray-800 dark:bg-gray-900 dark:bg-gray-100 shadow"
-v-if="$route.meta.title">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold leading-tight text-gray-900">{{ $route.meta.title }}</h1>
-      </div>
-    </header>
     <main>
       <router-view/>
     </main>
-  </div>
 </template>
